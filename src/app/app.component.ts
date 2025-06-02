6b8f51cd2ad0138edb33c9eccb1c61e1e90d49c8
Gersomsim <gersomhernandez@me.com>
2025-06-01 23:25:35 -0600

    Feat: reemplazar contenido de app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterOutlet } from '@angular/router'
+import { MainLayoutComponent } from './ui/layouts/main-layout/main-layout.component'
+import { initFlowbite } from 'flowbite'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet, MainLayoutComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Event-Manager';
+	title = 'Event-Manager'
+	ngOnInit() {
+		initFlowbite()
+	}
 }
