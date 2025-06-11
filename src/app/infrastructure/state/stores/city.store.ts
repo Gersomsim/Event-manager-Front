8b3386e4a71f218de6fdc29015a153530f486d23
Gersomsim <gersomhernandez@me.com>
2025-06-10 23:54:58 -0600

    Feat: agregar decorador Injectable a CityStore

--- src/app/infrastructure/state/stores/city.store.ts
+++ src/app/infrastructure/state/stores/city.store.ts
@@ -1,9 +1,11 @@
 import { City } from '@domain/entities'
 import { BaseStore } from './common/base.store'
+import { Injectable } from '@angular/core'
 
 /**
  * Store para la entidad City que maneja el estado de la aplicación
  */
+@Injectable({ providedIn: 'root' })
 export class CityStore extends BaseStore<City> {
 	constructor() {
 		super('city')
